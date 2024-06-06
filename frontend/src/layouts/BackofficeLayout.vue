73ec5d8d415d52977c6246e803450f4270242914
Lucie <lucie.godard1704@gmail.com>
2024-06-07 00:58:08 +0200

    ✨ add backoffice views and layout

---
+++ frontend/src/layouts/BackofficeLayout.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app-bar
+    title="Julu & Peer Admin"
+    color="gray"
+  >
+    <router-link to="/admin/users">
+      Utilisateurs
+    </router-link>    
+    <router-link to="/admin/products">
+      Produits
+    </router-link>
+  </v-app-bar>
+  <v-main>
+    <router-view />
+  </v-main>
+  <v-footer
+    app
+    color="blue"
+    class="d-flex flex-column"
+  >
+    <div class="text-center w-100">
+      {{ new Date().getFullYear() }} — <strong>ESGI</strong>
+    </div>
+  </v-footer>
+</template>
