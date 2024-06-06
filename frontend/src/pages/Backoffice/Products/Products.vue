001cf487fb48efb15ef04d04031cddcb6c00b5f9
Lucie <lucie.godard1704@gmail.com>
2024-06-07 00:58:08 +0200

    ✨ add backoffice views and layout

---
+++ frontend/src/pages/Backoffice/Products/Products.vue
@@ -0,0 +1,15 @@
+<route lang="yaml">
+    path: /admin/products
+    name: admin-products
+    meta:
+      layout: BackofficeLayout
+  </route>
+  
+<template>
+  <div>
+    <h1>
+      List of products soon
+    </h1>
+  </div>
+</template>
+  
---
+++ frontend/src/pages/Backoffice/Users/Users.vue
@@ -0,0 +1,15 @@
+<route lang="yaml">
+    path: /admin/users
+    name: admin-users
+    meta:
+      layout: BackofficeLayout
+  </route>
+  
+<template>
+  <div>
+    <h1>
+      List of users will be found here be patient 
+    </h1>
+  </div>
+</template>
+  
