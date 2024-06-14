4cf967779ea878733bc0db3775d5c80d4f41039f
Lucie <lucie.godard1704@gmail.com>
2024-06-14 17:18:12 +0200

    wip example

---
+++ frontend/src/pages/Shop/About/components/MyAboutList.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup>
+
+withDefaults(defineProps<{
+    list: Array<string>,
+    title?: string
+}>(), {
+    title: 'My About Card',
+});
+
+</script>
+
+<template>
+  <h2>{{ title }}</h2>
+  <ul>
+    <li
+      v-for="item in list"
+      :key="`key.${item}`"
+    >
+      <p>{{ item }}</p>
+    </li>
+  </ul>
+</template>
