0e62d90b625720669a7d870c7da9fdd816ce8cc3
Lucie <lucie.godard1704@gmail.com>
2024-07-16 12:01:47 +0200

    connect mongodb

--- backend/models/mongo/db.js
+++ backend/models/mongo/db.js
@@ -1,5 +1,10 @@
 const mongoose = require('mongoose');
 const logger = require('../../utils/logger');
+const dotenv = require('dotenv');
+
+if(process.env.NODE_ENV === 'staging'){
+  dotenv.config({ path: `./.env.staging` });
+}
 
 mongoose
   .connect(process.env.MONGO_URL)
