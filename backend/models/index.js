2a82b13cbb4586a73051936c17eca1331cd64244
Lucie <lucie.godard1704@gmail.com>
2024-07-16 13:24:22 +0200

    config postgres for deployment

--- backend/models/index.js
+++ backend/models/index.js
@@ -10,8 +10,8 @@ const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
 let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+if (process.env.DATABASE_URL) {
+  sequelize = new Sequelize(process.env.DATABASE_URL);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
