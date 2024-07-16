ec6a134afffff3c5f6fc5dfd4d9c8235a49925b1
Lucie <lucie.godard1704@gmail.com>
2024-07-16 12:15:33 +0200

    added mongo url to heroku env var

--- backend/models/mongo/db.js
+++ backend/models/mongo/db.js
@@ -1,10 +1,5 @@
 const mongoose = require('mongoose');
 const logger = require('../../utils/logger');
-const dotenv = require('dotenv');
-
-if(process.env.NODE_ENV === 'staging'){
-  dotenv.config({ path: `./.env.staging` });
-}
 
 mongoose
   .connect(process.env.MONGO_URL)
