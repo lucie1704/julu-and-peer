52b9a427d52e36bc0472aa88aa21e3e5d4da8fe3
justinDev91 <justin@sustainsoft.eu>
2024-07-19 23:40:03 +0200

    feat(email-notification): implement, newletter, stock, new product, new price notification.

--- backend/utils/email.js
+++ backend/utils/email.js
@@ -5,8 +5,8 @@ const {convert} = require('html-to-text');
 module.exports = class Email {
   constructor(user, url) {
     this.to = user.email;
-    this.firstName = user.firstname;
-    this.url = url;
+    this.firstName = user.name.split(' ')[0];
+    this.url = url || "";
     this.from = `${process.env.USERNAME} <${process.env.EMAIL_FROM}>`;
   }
 
@@ -40,6 +40,24 @@ module.exports = class Email {
       // await this.newTransport().sendMail(mailOptions);
     // }
   }
+
+  async sendNewsLetter(template, subject, data) {
+    const html = pug.renderFile(`${__dirname}/../views/email/newsletter/${template}.pug`, 
+     data
+    );
+
+    const mailOptions = {
+      from: this.from,
+      to: this.to,
+      subject,
+      html,
+    };
+
+
+    // if (process.env.NODE_ENV === 'production') {
+      // await this.newTransport().sendMail(mailOptions);
+    // }
+  }
   
   async sendPasswordUpdated() {
     await this.send(
@@ -51,7 +69,7 @@ module.exports = class Email {
   async sendWelcome() {
     await this.send(
     'welcome',
-    'Bienvenue chez Julu&Peer !'
+    'Welcome to the julu-and-peer!'
     );
   }
 
@@ -70,6 +88,27 @@ module.exports = class Email {
     await this.send(
       'confirmPasswordReset',
       'Your password has been reset successfully'
+    );
+  }
+
+  async newsLetters() {
+    await this.sendNewsLetter(
+      'newsletter',
+      'Julu-and-peer newsletters',
+      {
+        newProducts: [
+          { name: 'Produit A', description: 'Description du produit A', image: 'https://via.placeholder.com/150' },
+          { name: 'Produit B', description: 'Description du produit B', image: 'https://via.placeholder.com/150' }
+        ],
+        discountedProducts: [
+          { name: 'Produit C', oldPrice: 30, newPrice: 20, image: 'https://via.placeholder.com/150' },
+          { name: 'Produit D', oldPrice: 50, newPrice: 40, image: 'https://via.placeholder.com/150' }
+        ],
+        restockedProducts: [
+          { name: 'Produit E', description: 'Description du produit E', image: 'https://via.placeholder.com/150' },
+          { name: 'Produit F', description: 'Description du produit F', image: 'https://via.placeholder.com/150' }
+        ],
+      }
     );
   }
 
