506683183a4c1a924866b6d1bb2648ff1d8ab67b
justinDev91 <justin@sustainsoft.eu>
2024-07-21 02:23:39 +0200

    feat(debug): images seeders

--- backend/seeders/20240718151914-create-image.js
+++ backend/seeders/20240718151914-create-image.js
@@ -6,13 +6,13 @@ const { Product, ProductArtist, Image } = require('../models');
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    // Retrieve all products and artists
+    // Retrieve all products and artist
     const products = await Product.findAll({limite: 5});
-    const artists = await ProductArtist.findAll({limite: 5});
+    const artist = await ProductArtist.findOne({ limit: 1 });
 
     // Check that we have at least one product and one artist
-    if (products.length === 0 || artists.length === 0) {
-      throw new Error('No products or artists found');
+    if (products.length === 0) {
+      throw new Error('No products found');
     }
 
     // Create an array to store image records
@@ -21,8 +21,6 @@ module.exports = {
     // Loop through each product
     for (const product of products) {
       // Loop through each artist
-      for (const artist of artists) {
-        // Create 10 images for each product-artist combination
           images.push({
             id: uuidv7(),
             width: 1920,
@@ -36,7 +34,6 @@ module.exports = {
             productId: product.id,
             artistId: artist.id,
           });
-      }
     }
 
     // Insert image records into the database
