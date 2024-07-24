869f7fdbd9ab2f203627fbb236426feacf1b4d70
DA SILVA SOUSA Pedro <pedromiguel.dasilvasousa92@gmail.com>
2024-07-24 09:10:31 +0200

    Add CGU/CGV and random images for seeders

---
+++ frontend/src/pages/Shop/CGU.vue
@@ -0,0 +1,108 @@
+<route lang="yaml">
+  path: /cgu
+  name: cgu
+  meta:
+    layout: AppLayout
+  </route>
+
+  <script lang="ts" setup></script>
+
+<template>
+  <div class="container p-6 mx-auto bg-white rounded-lg shadow-md">
+    <h1 class="mb-8 text-3xl font-bold text-center">
+      Conditions Générales d'Utilisation du site Julu&Peer
+    </h1>
+
+    <div class="prose prose-lg max-w-none">
+      <h2 class="mb-8 text-2xl font-bold text-center">
+        Article 1 : Objet
+      </h2>
+      <p>
+        Les présentes conditions générales d'utilisation ont pour objet de définir les modalités et conditions d'utilisation du site internet www.juluandpeer.store, édité par la société Julu&Peer.
+      </p>
+
+      <h2 class="mt-4 mb-8 text-2xl font-bold text-center">
+        Article 2 : Acceptation des CGU
+      </h2>
+      <p>
+        En accédant et en utilisant le Site, l'utilisateur accepte sans réserve les présentes CGU. En cas de désaccord avec les termes des CGU, l'Utilisateur est prié de ne pas utiliser le Site.
+      </p>
+
+      <h2 class="mt-4 mb-8 text-2xl font-bold text-center">
+        Article 3 : Accès au Site
+      </h2>
+      <p>
+        Le Site est accessible gratuitement à tout Utilisateur disposant d'un accès à internet. Tous les coûts afférents à l'accès au Site, que ce soit les frais matériels, logiciels ou d'accès à internet, sont exclusivement à la charge de l'Utilisateur.
+      </p>
+
+      <h2 class="mt-4 mb-8 text-2xl font-bold text-center">
+        Article 4 : Services proposés
+      </h2>
+      <p>
+        Le Site propose aux Utilisateurs la vente en ligne de disques vinyle neufs. Les produits proposés sont exclusivement livrés en France métropolitaine.
+      </p>
+
+      <h2 class="mt-4 mb-8 text-2xl font-bold text-center">
+        Article 5 : Données personnelles
+      </h2>
+      <p>
+        Conformément au Règlement Général sur la Protection des Données (RGPD), Julu&Peer s'engage à protéger les données personnelles des Utilisateurs. Les données collectées sur le Site sont utilisées uniquement dans le cadre de la gestion des commandes et des relations commerciales. L'Utilisateur dispose d'un droit d'accès, de rectification et de suppression de ses données personnelles en contactant Julu&Peer à l'adresse email suivante : julupeervinyle@gmail.com.
+      </p>
+
+      <h2 class="mt-4 mb-8 text-2xl font-bold text-center">
+        Article 6 : Propriété intellectuelle
+      </h2>
+      <p>
+        Tous les éléments du Site, y compris les textes, images, sons et vidéos, sont protégés par le droit d'auteur et le droit de la propriété intellectuelle. Toute reproduction, représentation, modification, publication, adaptation de tout ou partie des éléments du Site, quel que soit le moyen ou le procédé utilisé, est interdite, sauf autorisation écrite préalable de Julu&Peer.
+      </p>
+
+      <h2 class="mt-4 mb-8 text-2xl font-bold text-center">
+        Article 7 : Responsabilité
+      </h2>
+      <p>
+        Julu&Peer s'efforce de fournir sur le Site des informations aussi précises que possible. Toutefois, Julu&Peer ne pourra être tenu responsable des omissions, des inexactitudes et des carences dans la mise à jour, qu'elles soient de son fait ou du fait des tiers partenaires qui lui fournissent ces informations.
+      </p>
+      <p>
+        Julu&Peer ne pourra être tenu responsable des dommages directs et indirects causés au matériel de l'Utilisateur, lors de l'accès au Site, et résultant soit de l'utilisation d'un matériel ne répondant pas aux spécifications indiquées, soit de l'apparition d'un bug ou d'une incompatibilité.
+      </p>
+
+      <h2 class="mt-4 mb-8 text-2xl font-bold text-center">
+        Article 8 : Liens hypertextes
+      </h2>
+      <p>
+        Le Site peut contenir des liens hypertextes vers d'autres sites présents sur le réseau internet. Les liens vers ces autres ressources font quitter le Site. Julu&Peer décline toute responsabilité quant au contenu de ces sites.
+      </p>
+
+      <h2 class="mt-4 mb-8 text-2xl font-bold text-center">
+        Article 9 : Cookies
+      </h2>
+      <p>
+        La navigation sur le Site est susceptible de provoquer l'installation de cookie(s) sur l'ordinateur de l'Utilisateur. Un cookie est un fichier de petite taille, qui ne permet pas l'identification de l'Utilisateur, mais qui enregistre des informations relatives à la navigation d'un ordinateur sur un site. Les données ainsi obtenues visent à faciliter la navigation ultérieure sur le Site, et ont également vocation à permettre diverses mesures de fréquentation.
+      </p>
+      <p>
+        Le refus d'installation d'un cookie peut entraîner l'impossibilité d'accéder à certains services. L'Utilisateur peut toutefois configurer son ordinateur pour refuser l'installation des cookies.
+      </p>
+
+      <h2 class="mt-4 mb-8 text-2xl font-bold text-center">
+        Article 10 : Modification des CGU
+      </h2>
+      <p>
+        Julu&Peer se réserve le droit de modifier les présentes CGU à tout moment. Les modifications sont opposables à l'Utilisateur dès leur mise en ligne. L'utilisation du Site postérieurement à la publication des modifications vaut acceptation des CGU modifiées par l'Utilisateur.
+      </p>
+
+      <h2 class="mt-4 mb-8 text-2xl font-bold text-center">
+        Article 11 : Droit applicable et juridiction compétente
+      </h2>
+      <p>
+        Tout litige en relation avec l'utilisation du Site est soumis au droit français. Il est fait attribution exclusive de juridiction aux tribunaux compétents de Paris.
+      </p>
+
+      <p>
+        Julu&Peer<br>
+        242 rue du faubourg saint-antoine 75012 paris<br>
+        +33600000000<br>
+        julupeervinyle@gmail.com
+      </p>
+    </div>
+  </div>
+</template>
