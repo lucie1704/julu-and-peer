30fdd1d8345e37b15afd9174f6c86cd14bdf5cd2
Lucie <lucie.godard1704@gmail.com>
2024-07-24 07:39:09 +0200

    add cors policies for juluandpeer.store

--- backend/app.js
+++ backend/app.js
@@ -72,13 +72,20 @@ if (process.env.NODE_ENV === 'development') {
     app.use(cors());
 }
 
+const corsOptions = {
+  origin: 'https://www.juluandpeer.store',
+  optionsSuccessStatus: 200
+};
+
 if (process.env.NODE_ENV !== 'development') {
-  app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://www.juluandpeer.store');
-    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-  });
+  app.use(cors(corsOptions));
+
+//   app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', 'https://www.juluandpeer.store');
+//     res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+//     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//     next();
+//   });
 }
 
 // Apply CSRF protection middleware
